3577015948e3911b445b32010a6c342cae40b067
suvojit213 <suvojitsengupta21@gmail.com>
2025-08-12 19:31:23 +0530

    fix: Specify version for Compose Kotlin plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0"
 }
 
 android {
