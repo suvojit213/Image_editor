4695a158a3592b092f48404ca7e916e71831a4bd
suvojit213 <suvojitsengupta21@gmail.com>
2025-08-12 19:38:52 +0530

    fix: Update dependency versions in app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation(platform("androidx.compose:compose-bom:2025.05.01"))
     implementation("androidx.compose.ui:ui")
@@ -69,7 +69,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Coil for image loading
-    implementation("io.coil-kt:coil-compose:3.3.0")
+    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
 
     // Activity Result API for picking images
     implementation("androidx.activity:activity-ktx:1.10.1")
